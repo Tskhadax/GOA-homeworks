938932b2e101fe88bc534da2c4f12023633d7280
Tskhadax <giorgitskhadadze509@gmail.com>
2024-08-31 16:45:05 +0400

    second commit

---
+++ day 01/lesson01.py
@@ -0,0 +1,6 @@
+name = "giorgi"
+surname = "Tskhadadze"
+age = "17"
+weight = "184,4"
+country = "Georgia"
+print ("hey my name is " + name + " " + surname + " i'm from " + country + " i'm " + age + " y.o. my weight is " + weight)
