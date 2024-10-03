232819500c1a3e91670f5aeb5bf0af02a8ceff27
Tskhadax <giorgitskhadadze509@gmail.com>
2024-10-03 12:47:57 +0400

    first commit

---
+++ day 08/homework/Exercise32.py
@@ -0,0 +1,8 @@
+#შეამოწმე, არის თუ არა მომხმარებლის რიცხვი დადებითი.
+
+number = int(input("Enter number: "))
+
+if number > 0:
+    print("{0} is positive number".format(number))
+else:
+    print("{0} is negative number".format(number))
