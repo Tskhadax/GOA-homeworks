1da2ea072791ff2cc0bc79588fc666b799141ad9
Tskhadax <giorgitskhadadze509@gmail.com>
2024-10-21 21:36:03 +0400

    first commit

--- day 14/homework/Exercise63.py
+++ day 14/homework/Exercise63.py
@@ -1,5 +1,8 @@
 #Write a program that calculates and prints the sum of squares of numbers from 1 to 5 using a for loop.
 
+sum = 0
+
 for i in range(1,6):
-    sum = i * (i+i)
+    sum = sum + (i * i)
+
 print(sum)
